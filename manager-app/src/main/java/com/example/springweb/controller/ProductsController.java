dc99122e3baa35b5711a44058ede200373573630
Dmitrii Korneev <kdmitry.de@gmail.com>
2024-03-21 11:17:56 +0100

    manager-app module

---
+++ manager-app/src/main/java/com/example/springweb/controller/ProductsController.java
@@ -0,0 +1,46 @@
+package com.example.springweb.controller;
+
+import com.example.springweb.controller.payload.NewProductPayload;
+import com.example.springweb.entity.Product;
+import com.example.springweb.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("catalogue/products")
+public class ProductsController {
+
+    private final ProductService productService;
+
+    @GetMapping("list")
+    public String getProductList(Model model){
+        model.addAttribute("products", this.productService.findAllProducts());
+        return "catalogue/products/list";
+    }
+
+    @GetMapping("create")
+    public String getNewProductPage(){
+        return "catalogue/products/new_product";
+    }
+
+    @PostMapping("create")
+    public String createProduct(@Valid NewProductPayload payload,
+                                BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()){
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
+    }
+}
---
+++ manager-app/src/main/java/com/example/springweb/controller/payload/NewProductPayload.java
@@ -0,0 +1,12 @@
+package com.example.springweb.controller.payload;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record NewProductPayload(
+        @NotNull(message = "{catalogue.product.create.errors.title_is_invalid}")
+        @Size(min = 2, max = 50, message = "{catalogue.product.create.errors.title_size_is_invalid}")
+        String title,
+        @Size(max = 1500, message = "{catalogue.product.create.errors.details_size_is_invalid}")
+        String details) {
+}
