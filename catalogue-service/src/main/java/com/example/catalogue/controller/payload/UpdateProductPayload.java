406de20b226153fbc9ab78f4e521e9f37f3d6372
Dmitrii Korneev <kdmitry.de@gmail.com>
2024-03-22 10:50:50 +0100

    REST API completed

---
+++ catalogue-service/src/main/java/com/example/catalogue/controller/payload/UpdateProductPayload.java
@@ -0,0 +1,12 @@
+package com.example.catalogue.controller.payload;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UpdateProductPayload(
+        @NotNull(message = "{catalogue.product.update.errors.title_is_invalid}")
+        @Size(min = 2, max = 50, message = "{catalogue.product.update.errors.title_size_is_invalid}")
+        String title,
+        @Size(max = 1500, message = "{catalogue.product.update.errors.details_size_is_invalid}")
+        String details) {
+}
