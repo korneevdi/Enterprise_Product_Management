ad63f2c330a7018d38353a9f309babb9ae4d662e
Dmitrii Korneev <kdmitry.de@gmail.com>
2024-03-25 10:09:26 +0100

    entity modified to connect to database

--- catalogue-service/src/main/java/com/example/catalogue/entity/Product.java
+++ catalogue-service/src/main/java/com/example/catalogue/entity/Product.java
@@ -1,5 +1,8 @@
 package com.example.catalogue.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +10,20 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(schema = "catalogue", name = "t_product")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "c_title")
+    @NotNull
+    @Size(min = 2, max = 50)
     private String title;
 
+    @Column(name = "c_details")
+    @Size(max = 1500)
     private String details;
 }
