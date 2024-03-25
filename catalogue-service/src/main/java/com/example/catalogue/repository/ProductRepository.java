e6d5604cae2903b75f26c1329b84fe615918656c
Dmitrii Korneev <kdmitry.de@gmail.com>
2024-03-25 16:30:28 +0100

    some changes in entity class

--- catalogue-service/src/main/java/com/example/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/com/example/catalogue/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.example.catalogue.repository;
 
 import com.example.catalogue.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-import java.util.Optional;
+public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {}
+    // alphabetical filtering, case insensitive
+    @Query(name = "Product.findAllByTitleLikeIgnoringCase", nativeQuery = true)
+    Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
+}
