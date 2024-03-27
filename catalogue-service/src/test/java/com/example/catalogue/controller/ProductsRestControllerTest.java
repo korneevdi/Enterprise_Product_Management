ec0f782091dd72a7dce95d3c16f4b58a34205205
Dmitrii Korneev <kdmitry.de@gmail.com>
2024-03-27 14:58:54 +0100

    manager-app tests added

---
+++ catalogue-service/src/test/java/com/example/catalogue/controller/ProductsRestControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.catalogue.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductsRestControllerTest {
+
+}
--- manager-app/src/test/java/com/example/manager/controller/ProductsControllerTest.java
+++ manager-app/src/test/java/com/example/manager/controller/ProductsControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.manager.controller;
 
+import com.example.manager.client.BadRequestException;
 import com.example.manager.client.ProductsRestClient;
 import com.example.manager.controller.payload.NewProductPayload;
 import com.example.manager.entity.Product;
@@ -10,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.ConcurrentModel;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -44,4 +47,28 @@ class ProductsControllerTest {
         verify(this.productsRestClient).createProduct("New good", "Description");
         verifyNoMoreInteractions(this.productsRestClient);
     }
+
+    @Test
+    @DisplayName("createProduct returns a page with errors if the request is invalid")
+    void createProduct_RequestIsInvalid_ReturnsProductFormWithErrors() {
+        // given
+        var payload = new NewProductPayload("   ", null);
+        var model = new ConcurrentModel();
+
+        doThrow(new BadRequestException(List.of("Error 1", "Error 2")))
+                .when(this.productsRestClient)
+                .createProduct("   ", null);
+
+        // call the tested method
+        var result = this.controller.createProduct(payload, model);
+
+        // check the result of testing
+        assertEquals("catalogue/products/new_product", result);
+        assertEquals(payload, model.getAttribute("payload"));
+        assertEquals(List.of("Error 1", "Error 2"), model.getAttribute("errors"));
+
+        verify(this.productsRestClient).createProduct("   ", null);
+        verifyNoMoreInteractions(this.productsRestClient);
+    }
+
 }
